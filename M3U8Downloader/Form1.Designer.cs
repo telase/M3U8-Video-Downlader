083b5845cd6ee8704848274c764c0728ad53b75d
telase <telase@msn.com>
2021-04-23 22:45:54 +0300

    Add files via upload

---
+++ M3U8Downloader/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace M3U8Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtM3U8 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bgDownload = new System.ComponentModel.BackgroundWorker();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveLocationBox = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dilSeçimiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.türkçeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtM3U8
+            // 
+            this.txtM3U8.Font = new System.Drawing.Font("Segoe UI", 8.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtM3U8.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.txtM3U8.Location = new System.Drawing.Point(23, 209);
+            this.txtM3U8.Name = "txtM3U8";
+            this.txtM3U8.Size = new System.Drawing.Size(597, 23);
+            this.txtM3U8.TabIndex = 10;
+            this.txtM3U8.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TxtM3U8_MouseClick);
+            this.txtM3U8.TextChanged += new System.EventHandler(this.TxtM3U8_TextChanged);
+            this.txtM3U8.Leave += new System.EventHandler(this.TxtM3U8_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "M3U8 URL:";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnDownload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDownload.Font = new System.Drawing.Font("Segoe UI", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDownload.ForeColor = System.Drawing.Color.Transparent;
+            this.btnDownload.Location = new System.Drawing.Point(23, 267);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(140, 33);
+            this.btnDownload.TabIndex = 11;
+            this.btnDownload.Text = "DOWNLOAD";
+            this.btnDownload.UseVisualStyleBackColor = false;
+            this.btnDownload.Click += new System.EventHandler(this.BtnDownload_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Transparent;
+            this.btnExit.Location = new System.Drawing.Point(480, 267);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(140, 33);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "ÇIKIŞ";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(154, 30);
+            this.label3.MaximumSize = new System.Drawing.Size(320, 10);
+            this.label3.MinimumSize = new System.Drawing.Size(300, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(313, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "TeLaSe M3U8 DOWNLOADER AVC";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // bgDownload
+            // 
+            this.bgDownload.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BgDownload_DoWork);
+            this.bgDownload.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BgDownload_RunWorkerCompleted);
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Font = new System.Drawing.Font("Segoe UI", 8.75F, System.Drawing.FontStyle.Italic);
+            this.textBox_Name.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBox_Name.Location = new System.Drawing.Point(23, 148);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(597, 23);
+            this.textBox_Name.TabIndex = 8;
+            this.textBox_Name.TextChanged += new System.EventHandler(this.TextBox_Name_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Video İsmi:";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CadetBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(513, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "İndirmeyi Sonlandır";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.CadetBlue;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(23, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 25);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Gözat";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // saveLocationBox
+            // 
+            this.saveLocationBox.Location = new System.Drawing.Point(103, 87);
+            this.saveLocationBox.MaximumSize = new System.Drawing.Size(517, 23);
+            this.saveLocationBox.MinimumSize = new System.Drawing.Size(517, 23);
+            this.saveLocationBox.Multiline = true;
+            this.saveLocationBox.Name = "saveLocationBox";
+            this.saveLocationBox.Size = new System.Drawing.Size(517, 23);
+            this.saveLocationBox.TabIndex = 6;
+            this.saveLocationBox.TextChanged += new System.EventHandler(this.SaveLocationBox_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "İndirme Klasörü";
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dilSeçimiToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(647, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dilSeçimiToolStripMenuItem
+            // 
+            this.dilSeçimiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.türkçeToolStripMenuItem,
+            this.englishToolStripMenuItem});
+            this.dilSeçimiToolStripMenuItem.Name = "dilSeçimiToolStripMenuItem";
+            this.dilSeçimiToolStripMenuItem.Size = new System.Drawing.Size(33, 20);
+            this.dilSeçimiToolStripMenuItem.Text = "Dil";
+            this.dilSeçimiToolStripMenuItem.Click += new System.EventHandler(this.dilSecimiToolStripMenuItem_Click);
+            // 
+            // türkçeToolStripMenuItem
+            // 
+            this.türkçeToolStripMenuItem.Name = "türkçeToolStripMenuItem";
+            this.türkçeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.türkçeToolStripMenuItem.Text = "Türkçe";
+            this.türkçeToolStripMenuItem.Click += new System.EventHandler(this.türkçeToolStripMenuItem_Click);
+            // 
+            // englishToolStripMenuItem
+            // 
+            this.englishToolStripMenuItem.Name = "englishToolStripMenuItem";
+            this.englishToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.englishToolStripMenuItem.Text = "English";
+            this.englishToolStripMenuItem.Click += new System.EventHandler(this.englishToolStripMenuItem_Click);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(156, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "© 2021";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(647, 323);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.saveLocationBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtM3U8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TeLaSe M3U8 Downloader AVC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtM3U8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker bgDownload;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox saveLocationBox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dilSeçimiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem türkçeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem englishToolStripMenuItem;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ M3U8Downloader/Form1.cs
@@ -0,0 +1,194 @@
+﻿using M3U8Downloader.Properties;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Windows.Forms;
+
+namespace M3U8Downloader
+{
+
+    public partial class Form1 : Form
+ {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Settings.Default.dil == "English")
+            {
+                Localization.Culture = new CultureInfo("en-US");
+            }
+            else
+            {
+                Localization.Culture = new CultureInfo("");
+            }
+            dilSeçimiToolStripMenuItem.Text = Localization.dilSecimi;
+            btnDownload.Text = Localization.btnDownload;
+            btnExit.Text = Localization.btnExit;
+            button1.Text = Localization.button1;
+            button3.Text = Localization.button3;
+            label1.Text = Localization.label1;
+            label2.Text = Localization.label2;
+            label4.Text = Localization.label4;
+        }
+
+        private FolderBrowserDialog FolderBrowserDialog;
+        private string SaveLocationBox_Text;
+
+        public Form1(string saveLocationBox_Text)
+        {
+            SaveLocationBox_Text = saveLocationBox_Text;
+        }
+
+        public Form1(TextBox SaveLocationBox)
+        {
+            this.saveLocationBox = SaveLocationBox;
+        }
+
+        public void ExecuteCommand(string command)
+        {
+
+            //Create process
+
+            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+            pProcess.StartInfo.FileName = "CMD.exe";
+            pProcess.StartInfo.Arguments = command;
+            pProcess.StartInfo.CreateNoWindow = true;
+            pProcess.StartInfo.UseShellExecute = false;
+            pProcess.StartInfo.RedirectStandardOutput = true;
+
+            //pProcess.OutputDataReceived += new DataReceivedEventHandler(SortOutputHandler);
+            // Start the asynchronous read
+            pProcess.Start();
+            pProcess.BeginOutputReadLine();
+            pProcess.WaitForExit();
+            pProcess.Close();
+        }
+       
+        
+
+
+        private void TxtM3U8_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtM3U8.SelectAll();
+        }
+
+        private void BtnDownload_Click(object sender, EventArgs e)
+        {
+            btnDownload.Text = "Downloading...";
+            bgDownload.RunWorkerAsync();
+        }
+
+public void Download()
+        {
+            string m3u8 = txtM3U8.Text;
+            var targetPath = Path.Combine(saveLocationBox.Text, textBox_Name.Text);
+            string cmd = "/C ffmpeg -i \"" + m3u8 + "\" -acodec copy -vcodec copy -absf aac_adtstoasc \"" + targetPath + ".mp4\"";
+            ExecuteCommand(cmd);
+        }
+
+        private void BgDownload_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Download();
+        }
+
+        private void BgDownload_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("FINIS");
+            btnDownload.Text = "DOWNLOAD";
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void TxtM3U8_TextChanged(object sender, EventArgs e)
+        {
+            txtM3U8.ForeColor = Color.Black;
+            txtM3U8.Font = new Font(txtM3U8.Font, FontStyle.Regular);
+        }
+
+        private void TxtM3U8_Leave(object sender, EventArgs e)
+        {
+            txtM3U8.ForeColor = SystemColors.GrayText;
+            txtM3U8.Font = new Font(txtM3U8.Font, FontStyle.Italic);
+        }
+
+        private void TextBox_Name_TextChanged(object sender, EventArgs e)
+        {
+            txtM3U8.SelectAll();
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                WorkingDirectory = @"c:\",
+                FileName = "SendSignal.exe",
+                Arguments = "ffmpeg.exe",
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            };
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog = new FolderBrowserDialog();
+            DialogResult = FolderBrowserDialog.ShowDialog();
+
+            saveLocationBox.Clear();
+            saveLocationBox.AppendText(FolderBrowserDialog.SelectedPath);
+            saveLocationBox.ReadOnly = true;
+        }
+
+        private void SaveLocationBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void türkçeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Settings.Default.dil = "Türkçe";
+            Settings.Default.Save();
+            Application.Restart();
+        }
+
+        private void englishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Settings.Default.dil = "English";
+            Settings.Default.Save();
+            Application.Restart();
+        }
+
+        private void dilSecimiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
