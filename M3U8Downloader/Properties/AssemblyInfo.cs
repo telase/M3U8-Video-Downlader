b6938b58f9b4839ebcdb4bce4f4e82c1b2712a2a
telase <telase@msn.com>
2021-04-23 23:03:03 +0300

    Add files via upload

---
+++ M3U8Downloader/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TeLaSe M3U8Downloader AVC")]
+[assembly: AssemblyDescription("TeLaSe M3U8Downloader AVC")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TeLaSe")]
+[assembly: AssemblyProduct("TeLaSe M3U8Downloader AVC")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("TeLaSe")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9a39d723-1d90-43e1-9b6b-8ddbd558c40f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.2.1")]
+[assembly: AssemblyFileVersion("2.0.2.1")]
